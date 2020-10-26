eb2010a06f093b666a84423035e19aace6c0d800
yyzcl <yyzclyang@outlook.com>
2020-10-26 23:56:10 +0800

    init

---
+++ example/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+
+const App = () => {
+  return (
+    <>
+      <h1>hello</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './example/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'x-router',
+      template: './example/index.html'
+    })
+  ]
+};

b5ef7ce7be11091358f8a2cbc7c3deb1b481aeef
yyzcl <yyzclyang@outlook.com>
2020-10-28 00:41:50 +0800

    feat(Example): 添加多级路由

--- example/about.js
+++ example/about.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link, Route } from 'x-router';
+import User from './user';
+import Company from './company';
 
 const About = () => {
-  return <div>about</div>;
+  return (
+    <div>
+      about
+      <div>
+        <Link to="/about/user">User</Link>
+        <Link to="/about/company">Company</Link>
+      </div>
+      <div>
+        <Route path="/about/user" component={User} />
+        <Route path="/about/company" component={Company} />
+      </div>
+    </div>
+  );
 };
 
 export default About;

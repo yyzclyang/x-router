2770334a11ecf7459caaccade11b2a570bfe383b
yyzcl <yyzclyang@outlook.com>
2020-10-27 20:28:25 +0800

    feat(Example): 添加示例

--- example/App.js
+++ example/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Home from './home';
+import About from './about';
+import { HashRouter as Router, Route } from 'x-router';
 import './app.css';
 
 const App = () => {
   return (
     <>
-      <h1>hello</h1>
+      <h1>X-Router</h1>
+      <Router>
+        <>
+          <Route path="/home" component={Home} />
+          <Route path="/about" component={About} />
+        </>
+      </Router>
     </>
   );
 };

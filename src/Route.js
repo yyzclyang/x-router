d973aebbec3697307962d9864b95ec061c12bbb9
yyzcl <yyzclyang@outlook.com>
2020-10-27 21:13:39 +0800

    feat(Route): 增加 exact 属性

--- src/Route.js
+++ src/Route.js
@@ -3,13 +3,13 @@ import Context from './context';
 import { matchPath } from './utils';
 
 const Route = props => {
-  const { path, component: Component } = props;
+  const { component: Component } = props;
   const [state] = useContext(Context);
   const {
     location: { pathname }
   } = state;
 
-  return matchPath(pathname, { path }) ? <Component /> : null;
+  return matchPath(pathname, props) ? <Component /> : null;
 };
 
 export default Route;

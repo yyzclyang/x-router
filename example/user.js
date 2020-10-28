bbc2db29a993ed179d8f72e6ba304dc7023f4698
yyzcl <yyzclyang@outlook.com>
2020-10-28 19:57:40 +0800

    feat(example): 添加userDetail

--- example/user.js
+++ example/user.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Route } from 'x-router';
+import UserDetail from './userDetail';
 
 const User = () => {
-  return <div>User</div>;
+  return (
+    <div>
+      User
+      <Route path="/about/user/:id" exact component={UserDetail} />
+    </div>
+  );
 };
 
 export default User;

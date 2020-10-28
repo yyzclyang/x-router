9fb8e63e047c9dfb61445b11622644a3d06b160d
yyzcl <yyzclyang@outlook.com>
2020-10-29 00:17:16 +0800

    refactor(ReactContext): 重构 ReactContext

--- src/RouterContext.js
+++ src/RouterContext.js
@@ -1,35 +1,6 @@
 import React from 'react';
-import { ChangePathname } from './type';
 
 const RouterContext = React.createContext(null);
-
-const initialState = {
-  history: {
-    push: to => {
-      window.location.hash = to;
-    }
-  },
-  location: { pathname: window.location.hash.slice(1) || '/' },
-  match: {}
-};
-
-const reducers = {
-  [ChangePathname]: (state, action) => {
-    return {
-      ...state,
-      location: { ...state.location, pathname: action.payload }
-    };
-  }
-};
-
-const reducer = (state, action) => {
-  const actionFn = reducers[action.type];
-  if (actionFn) {
-    return actionFn(state, action);
-  } else {
-    throw new Error('unknown type');
-  }
-};
+RouterContext.displayName = 'RouterContext';
 
 export default RouterContext;
-export { initialState, reducer };

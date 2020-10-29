5e57f156f13a6b3b486576a0f9be0ca95a060cab
yyzcl <yyzclyang@outlook.com>
2020-10-29 20:45:20 +0800

    feat(Example): 添加用例页面

--- example/App.js
+++ example/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { HashRouter as Router, Route, Link, Redirect, Switch } from 'x-router';
 import Home from './home';
+import Profile from './profile';
 import About from './about';
 import './app.css';
 
@@ -13,10 +14,12 @@ const App = () => {
           <div>
             <Link to="/home">home</Link>
             <Link to="/about">about</Link>
+            <Link to="/profile">profile</Link>
           </div>
           <div>
             <Switch>
               <Route path="/home" component={Home} />
+              <Route path="/profile" component={Profile} />
               <Route path="/about" component={About} />
               <Redirect to="/home" />
             </Switch>
--- example/userDetail.js
+++ example/userDetail.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useParams } from 'x-router/hooks';
 
 const UserDetail = () => {
-  return <div>user detail</div>;
+  const params = useParams();
+  return <div>user detail: {params.id}</div>;
 };
 
 export default UserDetail;
